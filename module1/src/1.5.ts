b1a2b65f4e81426c4843b9df578f49a1b5270dbb
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-05-03 19:25:15 +0600

    added 1.5 ts file

---
+++ module1/src/1.5.ts
@@ -0,0 +1,11 @@
+//Reference type ==> Object
+
+const user: {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+} = {
+  firstName: "Abdullah",
+  middleName: "Rafi",
+  lastName: "Bhuiyan",
+};
