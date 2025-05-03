3a9dd62463a34c0145159ad22bb2bf947266612c
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-05-03 18:07:25 +0600

    first commit

---
+++ module1/src/1.4.ts
@@ -0,0 +1,27 @@
+//Basic data type
+
+//string
+let firstName: string = "rafi";
+
+//number
+let roll: number = 122;
+
+//boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined;
+
+// null
+let y: null;
+
+//Array
+
+let friends: string[] = ["rachel", "monica"];
+
+//tuple ---> array ==> order ---> type of values
+let coordinates: [number, number] = [1, 3]; // 2 ta data k alada alada vabe define kora jay
+
+let ageName: [number, string, boolean] = [5, "rafi", true];
+
+ageName[0] = 25;
