1f9a5ef6a2a3f34d8a8b9f47dcdcb28127dc3013
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-05-06 19:25:12 +0600

    spread and rest operator added

--- module1/src/1.7.ts
+++ module1/src/1.7.ts
@@ -1,3 +1,44 @@
 // Spread Operator
 // Rest Operator
 // Destructuring
+
+// Spread Operator
+const bros1: string[] = ["Mir", "Firoz", "Mizan"];
+
+const bros2: string[] = ["Tonmoy", "Nahid", "Rahat"];
+
+// bros1.push(bros2)   //eivabe use kora jabena karon string er modde array rakha jayna
+bros1.push(...bros2);
+
+const mentors1 = {
+  typescript: "Mezba",
+  redux: "Mir",
+  dbms: "Mizan",
+};
+
+const mentors2 = {
+  prisma: "Firoz",
+  nextjs: "Tanmoy",
+  cloud: "Nahid",
+};
+
+const mentorList = {
+  ...mentors1,
+  ...mentors2,
+};
+
+//  Rest Operator ===>
+
+// const greetFriends = (friend1: string, friend2: string, friend3: string) => {
+//   console.log(`Hi ${friend1}, ${friend2}, ${friend3}`);
+// };
+
+const greetFriends = (...friends: string[]) => {
+  //   console.log(`Hi ${friend1}, ${friend2}, ${friend3}`);
+
+  friends.forEach((friend: string) => {
+    console.log(friend);
+  });
+};
+
+greetFriends("Abul", "Babul", "Tabul");
