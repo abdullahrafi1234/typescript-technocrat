a7b5300b153f25aaa93be2f59e3ca46e8213f055
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-05-07 13:58:58 +0600

    added nullable, never and unknown type

--- module1/src/1.12.ts
+++ module1/src/1.12.ts
@@ -1,6 +1,7 @@
 {
   // Never,unknown and nullable type
 
+  // nullable type
   const searchName = (value: string | null) => {
     if (value) {
       console.log("Searching");
@@ -11,7 +12,29 @@
 
   searchName(null);
 
-  //unknown type
+  // Unknown Type (type of)
+  const getSpeedInMeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertedSpeed = (value * 1000) / 3600;
+      console.log(`The speed is ${convertedSpeed}`);
+    }
 
-  //
+    if (typeof value === "string") {
+      const [result, unit] = value.split(` `);
+      const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else {
+      console.log("Wrong Input");
+    }
+  };
+
+  getSpeedInMeterPerSecond(`1000 kmh^-1`);
+
+  // Never Type
+  const throwError = (msg: string): never => {
+    throw new Error(msg);
+  };
+
+  throwError("mushkil hugaya");
 }
