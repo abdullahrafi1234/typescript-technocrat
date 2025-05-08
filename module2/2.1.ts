a09b0798f89b99def06dcb72ab4794f5f4642a26
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2025-05-08 18:28:12 +0600

    added assertion and type narrowing

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -7,4 +7,23 @@
   anything = 222;
 
   (anything as string).length;
+
+  const kgToGram = (value: string | number): string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return convertedValue;
+    }
+    if (typeof value === "number") {
+      return value * 1000;
+    }
+  };
+  const result1 = kgToGram(90) as number;
+  const result2 = kgToGram("890") as string;
+  console.log(result1);
+  console.log(result2);
+
+  try {
+  } catch (error) {
+    console.log(error.message);
+  }
 }
